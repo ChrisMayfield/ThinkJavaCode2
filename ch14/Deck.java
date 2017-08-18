bcd64d6d3e324a327bb4abf763074d966dc52772
Chris Mayfield <mayfiecs@jmu.edu>
2017-08-18 16:21:35 -0400

    reviewed ch14

--- ch14/Deck.java
+++ ch14/Deck.java
@@ -8,7 +8,6 @@ public class Deck extends CardCollection {
      */
     public Deck(String label) {
         super(label);
-
         for (int suit = 0; suit <= 3; suit++) {
             for (int rank = 1; rank <= 13; rank++) {
                 addCard(new Card(rank, suit));
