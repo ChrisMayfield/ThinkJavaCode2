b80ecd8efd614da09b95d8176857a5776a141a73
Chris Mayfield <mayfiecs@jmu.edu>
2019-09-02 22:32:25 -0400

    rename to Sim2 and Sim3

---
+++ ch15/Sim2.java
@@ -0,0 +1,53 @@
+import javax.swing.JFrame;
+
+/**
+ * Example simulation of moving objects.
+ */
+public class Sim2 {
+
+    /**
+     * Test program that draws a few polygons.
+     * 
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // create some regular polygons
+        MovingPolygon mp = new MovingPolygon(8, 30);
+        RotatingPolygon rp = new RotatingPolygon(5, 50);
+
+        // move them out of the corner
+        mp.translate(100, 100);
+        rp.translate(200, 200);
+
+        // create drawing, add polygons
+        Drawing drawing = new Drawing(800, 600);
+        drawing.add(mp);
+        drawing.add(rp);
+
+        // set up the window frame
+        JFrame frame = new JFrame("Drawing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(drawing);
+        frame.pack();
+        frame.setVisible(true);
+
+        // main simulation loop
+        while (true) {
+
+            // update the drawing
+            drawing.nextact();
+            drawing.repaint();
+
+            // delay the simulation
+            try {
+                Thread.sleep(1000 / 60);
+                frame.getToolkit().sync();
+            } catch (InterruptedException e) {
+                // do nothing
+            }
+        }
+    }
+
+}
--- ch15/Sim3.java
+++ ch15/Sim3.java
@@ -1,17 +1,21 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
- * Example simulation of moving objects.
+ * Smoother version using a timer.
  */
-public class Sim3 {
+public class Sim3 implements ActionListener {
+
+    private Drawing drawing;
+    private JFrame frame;
 
     /**
-     * Test program that draws a few polygons.
-     * 
-     * @param args command-line arguments
+     * Set up the drawing and window frame.
      */
-    public static void main(String[] args) {
-
+    public Sim3() {
         // create some regular polygons
         MovingPolygon mp = new MovingPolygon(8, 30);
         RotatingPolygon rp = new RotatingPolygon(5, 50);
@@ -21,33 +25,35 @@ public class Sim3 {
         rp.translate(200, 200);
 
         // create drawing, add polygons
-        Drawing drawing = new Drawing(800, 600);
+        drawing = new Drawing(800, 600);
         drawing.add(mp);
         drawing.add(rp);
 
         // set up the window frame
-        JFrame frame = new JFrame("Drawing");
+        frame = new JFrame("Drawing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.add(drawing);
         frame.pack();
         frame.setVisible(true);
+    }
 
-        // main simulation loop
-        while (true) {
+    /**
+     * Create and start the timer.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        Sim3 sim = new Sim3();
+        Timer timer = new Timer(1000 / 60, sim);
+        timer.start();
+    }
 
-            // update the drawing
-            drawing.nextact();
-            drawing.repaint();
-
-            // delay the simulation
-            try {
-                Thread.sleep(1000 / 60);
-                frame.getToolkit().sync();
-            } catch (InterruptedException e) {
-                // do nothing
-            }
-        }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        drawing.nextact();
+        drawing.repaint();
+        frame.getToolkit().sync();
     }
 
 }
