cea2427a7eaa6b79685e2fe74767eb0f520b444f
ndetloff <73507519+ndetloff@users.noreply.github.com>
2020-10-27 06:23:36 -0700

    Update Hello.java

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -6,3 +6,11 @@ public class Hello {
     }
 
 }
+
+public class Hello [
+    public static void main(String[]args) {
+        //generate some simple output
+        System.out.println("Hello, World!");
+    }
+    }
+    
