8fae7741ba0875a2aabb50b4029f2424d13d7853
ndetloff <73507519+ndetloff@users.noreply.github.com>
2020-10-27 06:39:37 -0700

    Update Hello2.java

--- ch01/Hello2.java
+++ ch01/Hello2.java
@@ -1,8 +1,8 @@
 public class Hello2{
     public static void main(String[]args) {
         //create some line of text
-        System.out.println ("Hello, World!"); //first line
-        System.out.println ("How are you?"); //second line
+        system.out.println ("Hello, World!"); //first line
+        system.out.println ("How are you?"); //second line
     }
 }
 
