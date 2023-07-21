cd4c4ea65e62d68e41b98e1dba7e75c3a7fe12aa
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-07-21 09:04:25 -0600

    Making it convert from inchs to centimeters as well

--- ch03/Convert.java
+++ ch03/Convert.java
@@ -12,6 +12,36 @@ public class Convert {
         final int IN_PER_FOOT = 12;
         Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter 1 to convert centimeters to feet and inches\n"
+                         + "Enter 2 to convert feet and inches to centimeters\n"
+                         + "Your choice: ");
+
+        int choice = in.nextInt();
+
+        if (choice == 1) {
+            System.out.print("Exactly how many cm? ");
+            cm = in.nextDouble();
+
+            if (cm < 0) {
+                System.out.print("Please enter a positive value for centimeters.");
+            } else {
+                inches = (int) (cm / CM_PER_INCH);
+                feet = inches / IN_PER_FOOT;
+                remainder = inches % IN_PER_FOOT;
+                inches = inches - (feet * IN_PER_FOOT);
+                System.out.printf("%.2f cm = %fd ft, %d in\n", cm, feet, inches);
+            }
+        } else if (choice == 2); {
+            System.out.print("Enter feet: ");
+            feet = in.nextInt();
+            System.out.print("Enter inches: ");
+            inches = in.nextInt();
+
+            if (feet < 0 || inches < 0 || inches >= IN_PER_FOOT) {
+                System.out.println("Please enter valid and positive values for feet and inches.");
+            }
+        }
+
         // prompt the user and get the value
         System.out.print("Exactly how many cm? ");
         cm = in.nextDouble();
