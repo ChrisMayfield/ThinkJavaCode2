c50483a0e350649012ea3468f54f21be1ccba64a
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-07-22 04:44:08 -0600

    Found/fixed error, fun!

--- ch03/Formatting.java
+++ ch03/Formatting.java
@@ -27,7 +27,7 @@ public class Formatting {
         inch = (int) (cm / CM_PER_INCH);
         System.out.printf("%f cm = %d in\n", cm, inch);
 
-        System.out.printf("inches = %d" + inch);  // error
+        System.out.printf("inches = %d", inch);  // Error was a '+' sign where a comma should have been, fixed!
     }
 
 }
